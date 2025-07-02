b07502f729752c35fa87bfcf146ec2dfc8381a2c
송지훈 <jihoon.song@medisolution.co.kr>
2025-07-02 14:25:40 +0900

    DB 연동 테스트

--- src/test/java/com/medi/testproject/TestProjectApplicationTests.java
+++ src/test/java/com/medi/testproject/TestProjectApplicationTests.java
@@ -1,13 +1,27 @@
 package com.medi.testproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootTest
 class TestProjectApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void testConnection() throws Exception {
+        try (Connection conn = dataSource.getConnection()) {
+            System.out.println("✅ DB 연결 성공: " + conn);
+        }
     }
 
 }
