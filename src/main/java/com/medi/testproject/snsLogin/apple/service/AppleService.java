0c834e6cadcef02b0480df8cfc529a8d82baacda
chrishoon <hooney1103@gmail.com>
2025-09-08 14:41:42 +0900

    [애플] response_type code로 변경

--- src/main/java/com/medi/testproject/snsLogin/apple/service/AppleService.java
+++ src/main/java/com/medi/testproject/snsLogin/apple/service/AppleService.java
@@ -1,10 +1,8 @@
 package com.medi.testproject.snsLogin.apple.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 
 @Slf4j
 @Service
@@ -19,24 +17,6 @@ public class AppleService {
     @Value("${apple.auth.uri}")
     private String APPLE_AUTH_URI;
 
-    @Value("${apple.team.id}")
-    private String APPLE_TEAM_ID;
-
-    @Value("${apple.login.key}")
-    private String APPLE_LOGIN_KEY;
-
-    // RestTemplate → deprecated 이슈로 RestClient로 대체
-    private final RestClient restClient;
-
-    // Jackson 라이브러리의 클래스. JSON <-> Java 객체 변환.
-    private final ObjectMapper objectMapper;
-
-    // 생성자 주입: objectMapper는 스프링에서 Bean으로 주입 가능.
-    public AppleService(ObjectMapper objectMapper) {
-        this.restClient = RestClient.create(); // RestClient 인스턴스 생성
-        this.objectMapper = objectMapper;
-    }
-
     /**
      *  로그인 URL 생성
      **/
@@ -45,7 +25,7 @@ public class AppleService {
         String loginUrl = APPLE_AUTH_URI + "/auth/authorize"
                 + "?client_id=" + APPLE_CLIENT_ID
                 + "&redirect_uri=" + APPLE_REDIRECT_URL
-                + "&response_type=code%20id_token&scope=name%20email&response_mode=form_post";
+                + "&response_type=code&scope=name%20email&response_mode=form_post";
 
         log.info("Access Token URL ::::: {}", loginUrl);
 
