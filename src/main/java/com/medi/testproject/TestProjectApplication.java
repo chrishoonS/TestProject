3fb6060278c304d3e8df5877aa53febf4ea0b27f
chrishoon <hooney1103@gmail.com>
2025-10-13 10:39:39 +0900

    [공통] DB 설정 주석 처리

--- src/main/java/com/medi/testproject/TestProjectApplication.java
+++ src/main/java/com/medi/testproject/TestProjectApplication.java
@@ -2,12 +2,12 @@ package com.medi.testproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class TestProjectApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/medi/testproject/TestProjectApplicationTests.java
+++ src/test/java/com/medi/testproject/TestProjectApplicationTests.java
@@ -1,11 +1,7 @@
 package com.medi.testproject;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
 
 @SpringBootTest
 class TestProjectApplicationTests {
@@ -14,14 +10,14 @@ class TestProjectApplicationTests {
     void contextLoads() {
     }
 
-    @Autowired
-    private DataSource dataSource;
+//    @Autowired
+//    private DataSource dataSource;
 
     @Test
     void testConnection() throws Exception {
-        try (Connection conn = dataSource.getConnection()) {
-            System.out.println("✅ DB 연결 성공: " + conn);
-        }
+//        try (Connection conn = dataSource.getConnection()) {
+//            System.out.println("✅ DB 연결 성공: " + conn);
+//        }
     }
 
 }
